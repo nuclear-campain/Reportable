e21434c20558cb2b9cfbdbfa841da176ec44e0da
Tim Joosten <Tjoosten3@gmail.com>
2018-12-01 20:59:50 +0100

    refactor: Remove strict support for the package.
    [SIGNED OFF]: Tim Joosten <topairy@gmail.com>

--- src/Traits/HasReports.php
+++ src/Traits/HasReports.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace BrianFaust\Reportable\Traits;
 
 use BrianFaust\Reportable\Models\Report;
--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace BrianFaust\Tests\Reportable;
 
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
